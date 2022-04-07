b98462a2fc1a5fe1453564ad30d5bdc56a00df46
Megan Krenbrink <megan.krenbrink@gmail.com>
2022-04-06 22:45:43 -0700

    store page api updates
    - Made updates to the fetch functions to call products in mom api
    - Changes added to store.html to properly display results
    - CSS updated to adjust layout of cards

--- js/main.js
+++ js/main.js
@@ -11,29 +11,45 @@ Table of Contents:
 ========================================*/
 function apiLoad() {
 
-  function getProducts() {
-    fetch('https://momapi.megankrenbrink.com/api/products/read.php?filter=')
+   async function getProducts() {
+    // create an empty array outside of the arrow function so it can be returned
+    let productsList = [];
+    await fetch('https://momapi.megankrenbrink.com/api/products/read.php?filter=produce')
       .then(res => res.json())
       .then(data => {
-        data.products;
+        // insert the products data in productList variable
+        productsList = data.products;
         console.log(data.products);
-      })
+      });
+      // return the populated productsList variable
+      return productsList;
   }
+
+  // function to display products on page
   async function renderProducts() {
     let data = await getProducts();
     let html = '';
     data.forEach(products => {
-      let htmlSegment = `<div class="card" style="width: 18rem;">
-      <img src="${products.image}" class="card-img-top" alt="${products.name}">
-      <div class="card-body">
-        <h5 class="card-title">${products.name}</h5>
-      </div>
-    </div>`;
+      let htmlSegment = 
+      `<div class="card">
+        <img src="${products.image}" class="card-img-top" alt="${products.name}">
+        <div class="card-body">
+          <p class="card-title">${products.name}</p>
+          <div class="d-flex justify-content-between">
+            <p class="card-text">${products.retail}</p>
+            <svg class="add-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#97C45F"
+              class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+              <path
+                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+            </svg>
+          </div>
+        </div>
+      </div>`
 
       html += htmlSegment;
     });
 
-    let container = document.querySelector('.main');
+    let container = document.querySelector('.products');
     container.innerHTML = html;
   }
 
